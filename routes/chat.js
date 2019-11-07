4daefdd7e33e1d9314ebb4c950d65038e366e618
Rigo-Hernandez <hernandez_rigoberto@outlook.com>
2019-11-06 20:00:00 -0600

    Updated Dependacies

--- routes/chat.js
+++ routes/chat.js
@@ -1,39 +1,30 @@
-const express =require('express');
+const express = require('express');
 const router = express.Router();
 
-
-
-
-router.get('/chat', (req, res)=>{
-    var role = ''
-    if (req.user){
-        if (req.user.role_id === 1){
-            role = 'teacher'
-        }else if (req.user.role_id === 2){
-            role = 'student' 
-        }else{
-            role = 'mentor'
-        }
-        res.render('chat', {
-            publicProfile: '/'+ role + '/'+ req.user.username,
-            user:req.user,
-            fName: req.user.fname,
-            mainUserName:req.user.fname,
-            fname: req.user.fname,
-            lname: req.user.lname,
-            profilepic: req.user.image_url,
-            username: req.user.username,
-            userid:req.user.id
-
-        })
-    }else{
-        res.redirect('/login')
+router.get('/chat', (req, res) => {
+  var role = '';
+  if (req.user) {
+    if (req.user.role_id === 1) {
+      role = 'teacher';
+    } else if (req.user.role_id === 2) {
+      role = 'student';
+    } else {
+      role = 'mentor';
     }
-
+    res.render('chat', {
+      publicProfile: '/' + role + '/' + req.user.username,
+      user: req.user,
+      fName: req.user.fname,
+      mainUserName: req.user.fname,
+      fname: req.user.fname,
+      lname: req.user.lname,
+      profilepic: req.user.image_url,
+      username: req.user.username,
+      userid: req.user.id
+    });
+  } else {
+    res.redirect('/login');
+  }
 });
 
-
-
-
-
-module.exports = router
+module.exports = router;
