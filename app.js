597393ff61f5105d176ea50e2c9de694787c27ad
Rigo-Hernandez <hernandez_rigoberto@outlook.com>
2019-11-07 23:39:32 -0600

    Updated Dependacies

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const db = require('./models/');
 const session = require('express-session');
 // const fileUpload = require('express-fileupload');
 
-
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(express.static('public'));
 
@@ -24,53 +23,44 @@ app.use(require('./routes/search'));
 app.use(require('./routes/cards'));
 const fileUpload = require('express-fileupload');
 
-
-app.get('/chat', (req, res)=>{
-    // res.sendFile(__dirname + '/views/index.ejs')
-    res.render('chat')
+app.get('/chat', (req, res) => {
+  // res.sendFile(__dirname + '/views/index.ejs')
+  res.render('chat');
 });
-app.use(require('./routes/friends'))
+app.use(require('./routes/friends'));
 app.use(require('./routes/api'));
 
 users = [];
 connections = [];
-var userid = `<%=userid%>`
-io.on('connection', function (socket) {
+var userid = `<%=userid%>`;
+io.on('connection', function(socket) {
+  connections.push(socket);
 
-    connections.push(socket);
+  socket.on('chat message', msg => {
+    socket.broadcast.emit('chat message', msg);
+  });
 
-    socket.on('chat message', (msg) => {
-        socket.broadcast.emit('chat message', msg)
-    });
-
-    socket.on('send-nickname', function(socket){
-        socket.nickname = nickanme;
-        userid.push(socket.nickname);
-        console.log(users)
-    })
-    console.log('Connected: %s sockets connected', connections.length);
+  socket.on('send-nickname', function(socket) {
+    socket.nickname = nickanme;
+    userid.push(socket.nickname);
+    console.log(users);
+  });
+  console.log('Connected: %s sockets connected', connections.length);
+  io.sockets.emit('totalUsers', { count: connections.length });
+  //Disconnect
+  socket.on('disconnect', function(data) {
+    users.splice(users.indexOf(socket.username), 1);
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets connected', connections.length);
     io.sockets.emit('totalUsers', { count: connections.length });
-    //Disconnect
-    socket.on('disconnect', function (data) {
-        users.splice(users.indexOf(socket.username), 1);
-        connections.splice(connections.indexOf(socket), 1);
-        console.log('Disconnected: %s sockets connected', connections.length);
-        io.sockets.emit('totalUsers', { count: connections.length });
-
-    });
-})
-
-
+  });
+});
 
 // need this only when creating database.
 // db.sequelize.sync({force:true}).then(()=>{
 //     app.listen(3500)
 // })
 
-
-
-
-
-http.listen(3000, ()=>{
-    console.log('listening on port 3000')
-})
+http.listen(3000, () => {
+  console.log('listening on port 3000');
+});
